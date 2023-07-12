5407e752f36f79ca5c5c0efabc54624678468866
Rodrigo <rodrigoursi@gmail.com>
2023-07-12 18:33:18 -0300

    Maquetie la pantalla cargar turno para empezar con datos reales

---
+++ clinicaMedica/Pages/cargarTurno.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="cargarTurno.aspx.cs" Inherits="clinicaMedica.Pages.cargarTurno" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="../Css/cargarTurno.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div>
+        <i class="fa-regular fa-calendar-days"></i>
+        <h3 style="display:inline-block">Turnos</h3>
+    </div>
+    <p>Completa el siguiente formulario para reservar el turno</p>
+    <hr />
+    <h4>Datos del paciente</h4>
+    <div class="formulario-turno mt-4">
+        <div class="mb-3 subcontenedor">
+            <div>
+                <label for="cargarTurno_documento" class="form-label">Documento</label>
+                <asp:TextBox runat="server" type="number" CssClass="form-control" id="cargarTurno_documento" />
+            </div>
+            <asp:Button Text="buscar" runat="server" class="btn btn-success" ID="buscarDoc" />
+        </div>
+        <div class="mb-3">
+            <label for="cargarTurno_documento" class="form-label">Paciente</label>
+            <asp:TextBox runat="server" CssClass="form-control" id="cargarTurno_paciente" />
+        </div>
+        <div class="mb-3 subcontenedor">
+            <div>
+                <label for="cargaTurno_esp" class="form-label">Especialidad</label>
+                <asp:DropDownList ID="cargaTurno_Esp" CssClass="form-select mb-3" runat="server"></asp:DropDownList>
+            </div>
+            <div>
+                <label for="cargaTurno_prof" class="form-label">Profecional</label>
+                <asp:DropDownList ID="cargaTurno_prof" CssClass="form-select mb-3" runat="server"></asp:DropDownList>
+            </div>
+        </div>
+        <div class="mb-3">
+            <div>
+                <label for="cargaTurno_fecha" class="form-label">Fecha</label>
+                <asp:DropDownList ID="cargaTurno_fecha" CssClass="form-select mb-3" runat="server"></asp:DropDownList>
+            </div>
+            <div>
+                <label for="cargaTurno_hora" class="form-label">Hora</label>
+                <asp:DropDownList ID="cargaTurno_hora" CssClass="form-select mb-3" runat="server"></asp:DropDownList>
+            </div>
+        </div>
+        <div class="mb-3">
+            <label for="cargarTurno_mot" class="form-label">Motivo/s de la Consulta</label>
+            <asp:TextBox runat="server" TextMode="MultiLine" id="cargarTurno_mot" CssClass="form-control" rows="3" />
+        </div>
+    </div>
+</asp:Content>
---
+++ clinicaMedica/Pages/cargarTurno.aspx.designer.cs
@@ -0,0 +1,89 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace clinicaMedica.Pages
+{
+
+
+    public partial class cargarTurno
+    {
+
+        /// <summary>
+        /// Control cargarTurno_documento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox cargarTurno_documento;
+
+        /// <summary>
+        /// Control buscarDoc.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button buscarDoc;
+
+        /// <summary>
+        /// Control cargarTurno_paciente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox cargarTurno_paciente;
+
+        /// <summary>
+        /// Control cargaTurno_Esp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList cargaTurno_Esp;
+
+        /// <summary>
+        /// Control cargaTurno_prof.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList cargaTurno_prof;
+
+        /// <summary>
+        /// Control cargaTurno_fecha.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList cargaTurno_fecha;
+
+        /// <summary>
+        /// Control cargaTurno_hora.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList cargaTurno_hora;
+
+        /// <summary>
+        /// Control cargarTurno_mot.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox cargarTurno_mot;
+    }
+}
